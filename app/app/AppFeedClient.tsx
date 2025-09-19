4e869351d78c0f8f5c341c115569865cc26fbf49
lernex <lernexbusiness@gmail.com>
2025-09-19 00:59:21 -0600

    Furthur bug fixing

--- app/app/AppFeedClient.tsx
+++ app/app/AppFeedClient.tsx
@@ -1,6 +1,7 @@
 "use client";
 import FypFeed from "@/components/FypFeed";
 import SubjectChips from "@/components/SubjectChips";
+import ClassPicker from "@/components/ClassPicker";
 import Link from "next/link";
 import { useLernexStore } from "@/lib/store";
 
@@ -8,6 +9,9 @@ export default function AppFeedClient() {
   const { selectedSubjects } = useLernexStore();
   return (
     <main className="min-h-[calc(100vh-56px)]">
+      <div className="pointer-events-none fixed right-3 top-[72px] z-40 sm:right-6">
+        <div className="pointer-events-auto"><ClassPicker /></div>
+      </div>
       <SubjectChips />
       {!selectedSubjects.length && (
         <div className="max-w-md mx-auto px-4 pb-1 text-center text-neutral-300 text-sm">
